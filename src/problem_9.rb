9795cf98a8e4aaae75b48ef7dc8bf25662e3b3fd
John Nelson <jnelson@johncoder.com>
2019-05-12 15:38:39 -0400

    Simplifying problem 9

--- src/problem_9.rb
+++ src/problem_9.rb
@@ -1,23 +1,28 @@
 #!/usr/bin/env ruby
 def problem_9(final_value)
-  limit = final_value
+  limit = 100000
+  attempts = 1
   a, b = 1, 2
 
   until final_value == Math.sqrt(a**2 + b**2) + a + b
-    if b == limit
+    if 2 * b + a < final_value
+      b += 1
+    elsif a + b >= final_value - a - b
       a += 1
       b = a + 1
-    elsif b < limit
-      b += 1
-    else
-      raise "No such luck: a=#{a} b=#{b}"
+    end
+
+    attempts += 1
+    if a >= (final_value / 3) or attempts > limit
+      raise "No such luck: a=#{a} b=#{b}, attempts=#{attempts}"
     end
   end
 
   c = final_value - a - b
 
-  p "a=#{a} b=#{b} c=#{c}"
+  p "a=#{a} b=#{b} c=#{c}, attempts=#{attempts}"
 
+  # NOTE(john): A little self doubt
   if a**2 + b**2 != c**2
     raise "doesn't work"
   end
@@ -25,4 +30,6 @@ def problem_9(final_value)
   return a * b * c
 end
 
+p problem_9(3 + 4 + 5)
 p problem_9 1000
+# p problem_9 1096  # fails after 99919 attempts
